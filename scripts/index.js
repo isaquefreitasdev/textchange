14aaa38b63d4beb829af26ef033654fccd117b68
isaquefreitasdev <84482119+isaquefreitasdev@users.noreply.github.com>
2023-02-15 10:50:27 -0300

    design prototipo pronto

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+let out  = document.getElementById("output_data")
+let mai = document.getElementById("M");
+let min = document.getElementById("m");
+
+
+function M(){
+    let entrada = document.getElementById("open_data");
+    document.getElementById("out")
+
+}
