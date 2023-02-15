7cc8838ebf394e83e3f6475ce9c47fe1dc5993b3
isaquefreitasdev <84482119+isaquefreitasdev@users.noreply.github.com>
2023-02-15 11:58:10 -0300

    readme e completo

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1>Text Change</h1>
+<p>Mude o seu texto de forma simples</p>
+<h2>Sobre
+<p>Ainda falta invalidar a entrada de números, mas a ideia principal que era transformar o texo colocado foi cumprida
+<h3>Tecnlogias e Ferramentas
+<li>HTML
+<li>CSS
+<li>JS
+<li>Figma
+<li>Fonts Google
+<li>Adobe Color
+<h4>Link:
+<a href="">
--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,58 @@
-let out  = document.getElementById("output_data")
 let mai = document.getElementById("M");
 let min = document.getElementById("m");
 
 
 function M(){
-    let entrada = document.getElementById("open_data");
-    document.getElementById("out")
+    let entrada = document.getElementById("open_data").value;
+    let out  = document.getElementById("output_data");
+    
+    let upper = entrada.toUpperCase();
+    ApenasLetras();
+    if(entrada.length < 1){
+        alert("Preencha para executar")
+    }else{
+        out.value = upper
 
+    }
+
+
+
+}
+function m(){
+    let entrada = document.getElementById("open_data").value;
+    let out  = document.getElementById("output_data");
+    
+    let lower = entrada.toLowerCase();
+    ApenasLetras();
+    if(entrada.length < 1){
+        alert("Preencha para executar")
+    }else{
+        out.value = lower
+
+    }
+    
+
+
+}
+function ApenasLetras(e, t) {
+    try {
+        if (window.event) {
+            var charCode = window.event.keyCode;
+        } else if (e) {
+            var charCode = e.which;
+        } else {
+            return true;
+        }
+        if (
+            (charCode > 64 && charCode < 91) || 
+            (charCode > 96 && charCode < 123) ||
+            (charCode > 191 && charCode <= 255) // letras com acentos
+        ){
+            return true;
+        } else {
+            return false;
+        }
+    } catch (err) {
+        alert(err.Description);
+    }
 }
